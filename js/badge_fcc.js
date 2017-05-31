142443ebc321568ecd06a3cbcf641809937b9cf9
Emma J <codemzy@gmail.com>
2017-05-31 16:28:41 +0000

    Added js for fcc badge

---
+++ js/badge_fcc.js
@@ -0,0 +1,32 @@
+/* global $ */
+
+$(document).ready(function() {
+    
+    var name = "codemzy";
+    // get name from url parameter
+    
+    // request user data from FCC api
+    var url = 'https://www.freecodecamp.com/' + name;
+    $.get(url, function(response) {
+        var score = $(response).find('.flat-top.text-primary').text().split(' ')[1];
+        $('#points').text(parseInt(score, 10));
+        var challenges = $(response).find('tbody tr').length;
+        $('#challenges').text(challenges);
+        // get the dates from the first item in each table
+        var date = $(response).find('tbody').find('tr:first td:eq(1)');
+        var dates = []; // array to hold years
+        for (var i = 0; i < 3; i++) {
+            // get the year
+            dates.push(parseInt($(date[i]).text().split(",")[1]));
+        }
+        // sort lowest first
+        dates.sort(function(a, b) {
+          return a - b;
+        });
+        // update the date with the lowest date
+        $('#date').text(dates[0]);
+    });
+
+    
+    
+});
