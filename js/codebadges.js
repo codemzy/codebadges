dc0b787574c8c97cf0b09559acf0093bc4abddaf
Emma J <codemzy@gmail.com>
2017-06-05 15:35:25 +0000

    Set up prototype

--- js/codebadges.js
+++ js/codebadges.js
@@ -7,10 +7,18 @@
         return new codeBadges.init(name);
     };
     
+    // Methods that can be used
+    codeBadges.prototype = {
+        
+    };
+    
     // codeBadges.init function constructor
     codeBadges.init = function(name) {
         var self = this;
         self.name = name || 'codemzy';
     };
+    
+    // Greetr init prototype same as the Greetr one (both point to the same prototype)
+    codeBadges.init.prototype = codeBadges.prototype;
     
 })(window, jQuery);
