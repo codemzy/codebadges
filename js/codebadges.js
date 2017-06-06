3cc6e21d265e9e47778232199a10da5a09bcdf86
Emma J <codemzy@gmail.com>
2017-06-06 08:23:10 +0000

    Updated github to api function

--- js/codebadges.js
+++ js/codebadges.js
@@ -48,7 +48,15 @@
         });
     };
     
-    
+    var gitHubAPI = function(name, callback) {
+        var url = 'https://api.github.com/users/' + name;
+        $.get(url, function(response) {
+            var followers = response.followers;
+            var repos = response.public_repos;
+            var date = response.created_at.split("-")[0];
+            callback({ top: followers, bottom: repos, date: date });
+        });
+    };
     
     // Methods that can be used
     codeBadges.prototype = {
@@ -86,19 +94,16 @@
         },
         
         // GitHub badge
-        gitHub: function() {
-            var name = this.name; // name already passed to init
-            // request user data from GitHub API
-            var url = 'https://api.github.com/users/' + name;
-            $.get(url, function(response) {
-                var followers = response.followers;
-                var repos = response.public_repos;
-                var date = response.created_at.split("-")[0];
+        gitHub: function(newName) {
+            // get the name 
+            var name = newName || this.name; // defaults to name passed to init
+            // call api function
+            gitHubAPI(name, function(data) {
                 // update the inner html of badge with all the info
-                var html = '<div id="points">' + followers + '</div><div class="small">followers</div>' +
+                var html = '<div id="points">' + data.top + '</div><div class="small">followers</div>' +
                 '<div id="user">' + name + '</div><div class="small">GitHub User</div>' +
-                '<div id="completed" class="small">Created</div><div><span id="challenges">' + repos + '</span> public repos</div>' +
-                '<div class="small">since <span id="date">' + date + '</span></div>';
+                '<div id="completed" class="small">Created</div><div><span id="challenges">' + data.bottom + '</span> public repos</div>' +
+                '<div class="small">since <span id="date">' + data.date + '</span></div>';
                 $('.code-badge.gh .inner').html(html);
             });
             return this; // return this so can chain methods
