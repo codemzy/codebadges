434e550be616feafa17f2c810f9bd580c97329cf
Emma J <codemzy@gmail.com>
2017-06-06 08:10:14 +0000

    Added freecodecamp api function

--- js/codebadges.js
+++ js/codebadges.js
@@ -26,6 +26,28 @@
         });
     };
     
+    var freeCodeCampAPI = function(name, callback) {
+        var url = 'https://www.freecodecamp.com/' + name; // request user data from FCC (no api so scraping off user page)
+        $.get(url, function(response) {
+            var score = $(response).find('.flat-top.text-primary').text().split(' ')[1];
+            var points = parseInt(score, 10); // get the users points
+            var challenges = $(response).find('tbody tr').length; // number of challenges completed
+            // get the dates from the first item in each table
+            var dateArr = $(response).find('tbody').find('tr:first td:eq(1)');
+            var dates = []; // array to hold years
+            for (var i = 0; i < 3; i++) {
+                // get the year
+                dates.push(parseInt($(dateArr[i]).text().split(",")[1], 10));
+            }
+            // sort lowest first
+            dates.sort(function(a, b) {
+              return a - b;
+            });
+            var date = dates[0];
+            callback({ top: points, bottom: challenges, date: date });
+        });
+    };
+    
     
     
     // Methods that can be used
@@ -34,7 +56,7 @@
         // CodeSchool badge
         codeSchool: function(newName) {
             // get the name 
-            var name = newName || this.name;
+            var name = newName || this.name; // defaults to name passed to init
             // call api function
             codeSchoolAPI(name, function(data) {
                 // update the inner html of badge with all the info
@@ -48,31 +70,16 @@
         },
         
         // FreeCodeCamp badge
-        freeCodeCamp: function() {
-            var name = this.name; // name already passed to init
-            // request user data from FCC (no api so scraping off user page)
-            var url = 'https://www.freecodecamp.com/' + name;
-            $.get(url, function(response) {
-                var score = $(response).find('.flat-top.text-primary').text().split(' ')[1];
-                var points = parseInt(score, 10); // get the users points
-                var challenges = $(response).find('tbody tr').length; // number of challenges completed
-                // get the dates from the first item in each table
-                var dateArr = $(response).find('tbody').find('tr:first td:eq(1)');
-                var dates = []; // array to hold years
-                for (var i = 0; i < 3; i++) {
-                    // get the year
-                    dates.push(parseInt($(dateArr[i]).text().split(",")[1], 10));
-                }
-                // sort lowest first
-                dates.sort(function(a, b) {
-                  return a - b;
-                });
-                var date = dates[0];
+        freeCodeCamp: function(newName) {
+            // get the name 
+            var name = newName || this.name; // defaults to name passed to init
+            // call api function
+            freeCodeCampAPI(name, function(data) {
                 // update the inner html of badge with all the info
-                var html = '<div id="points">' + points + '</div><div class="small">points</div>' +
+                var html = '<div id="points">' + data.top + '</div><div class="small">points</div>' +
                 '<div id="user">' + name + '</div><div class="small">FreeCodeCamp Student</div>' +
-                '<div id="completed" class="small">Completed</div><div><span id="challenges">' + challenges + '</span> challenges</div>' +
-                '<div class="small">since <span id="date">' + date + '</span></div>';
+                '<div id="completed" class="small">Completed</div><div><span id="challenges">' + data.bottom + '</span> challenges</div>' +
+                '<div class="small">since <span id="date">' + data.date + '</span></div>';
                 $('.code-badge.fcc .inner').html(html);
             });
             return this; // return this so can chain methods
