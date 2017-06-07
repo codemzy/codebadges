7715d1e501c1d54737755e1186724dfa04f7d22c
Emma J <codemzy@gmail.com>
2017-06-07 11:00:15 +0000

    Updated index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/* global $, codeBadges */
+
+$(document).ready(function() {
+    
+    // activate your codebadges with one line of code
+    codeBadges("codemzy").freeCodeCamp().codeSchool("bijanbwb").gitHub().codecademy().treeHouse("john").codeWars();
+    
+});
